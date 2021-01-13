7293cc9e07fcdcdf7344700e351809a899614b3d
w337886915 <wangheng19851019@126.com>
2021-01-13 16:59:36 +0800

    增加服务

---
+++ src/Providers/LaravelLoginServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace w337886915\app\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLoginServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
